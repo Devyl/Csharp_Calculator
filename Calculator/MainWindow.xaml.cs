00d442921f614d3c4dd69dcb350e8a98c096b45a
Hubert Tuleja <hubert.tuleja@gmail.com>
2019-08-11 21:18:59 +0200

    Dodaj pliki projektów.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+        }
+        public string toDo = string.Empty;
+        string a = "0";
+        //string b = string.Empty;
+        char whatchar = ' ';
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (a == "0")
+                display.Text = toDo;
+            else if (whatchar == '=')
+            {
+                display.Text = a;
+            }
+            else
+                display.Text = a + whatchar + toDo;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //1
+        {
+            if(toDo.Length<5)
+            toDo += "1";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //2
+        {
+            if (toDo.Length < 5)
+                toDo += "2";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) //3
+        {
+            if (toDo.Length < 5)
+                toDo += "3";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e) //CE
+        {
+            a = "0";
+            //b = string.Empty;
+            toDo = string.Empty;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e) //Del1
+        {
+            toDo = toDo.Remove(toDo.Length - 1);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e) //4
+        {
+            if (toDo.Length < 5)
+                toDo += "4";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e) //5
+        {
+            if (toDo.Length < 5)
+                toDo += "5";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e) //6
+        {
+            if (toDo.Length < 5)
+                toDo += "6";
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e) //*
+        {
+            if (!(a == "0"))
+            {
+                
+                a = calc.CharCheck(whatchar, a, toDo);
+                toDo = string.Empty;
+                whatchar = '*';
+            }
+            else
+            {
+                whatchar = '*';
+                a = toDo;
+                toDo = string.Empty;
+            }
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e) ///
+        {
+            if (!(a == "0"))
+            {
+                
+                a = calc.CharCheck(whatchar, a, toDo);
+                toDo = string.Empty;
+                whatchar = '/';
+
+            }
+            else
+            {
+                whatchar = '/';
+                a = toDo;
+                toDo = string.Empty;
+            }
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e) //7
+        {
+            if (toDo.Length < 5)
+                toDo += "7";
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e) //8
+        {
+            if (toDo.Length < 5)
+                toDo += "8";
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e) //9
+        {
+            if (toDo.Length < 5)
+                toDo += "9";
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e) //+
+        {
+            if (!(a == "0"))
+            {
+                
+                a = calc.CharCheck(whatchar, a, toDo);
+                toDo = string.Empty;
+                whatchar = '+';
+
+            }
+            else
+            {
+                whatchar = '+';
+                a = toDo;
+                toDo = string.Empty;
+            }
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e) //-
+        {
+            if (!(a == "0"))
+            {
+                
+                a = calc.CharCheck(whatchar, a, toDo);
+                toDo = string.Empty;
+                whatchar = '-';
+
+            }
+            else
+            {
+                whatchar = '-';
+                a = toDo;
+                toDo = string.Empty;
+            }
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e) //=
+        {
+            a = calc.CharCheck(whatchar, a, toDo);
+            whatchar = '=';
+
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e) //0
+        {
+            if (toDo.Length < 5)
+                toDo += "0";
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e) //,
+        {
+
+        }
+
+        private void Button_Click_18(object sender, RoutedEventArgs e) //Freebutton
+        {
+
+        }
+
+        private void Button_Click_19(object sender, RoutedEventArgs e) //Freebutton
+        {
+
+        }
+    }
+}
---
+++ Calculator/calc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class calc
+    {
+       static public string Add(string a, string b)
+        {
+            int sum = 0;
+            int c = Convert.ToInt32(a);
+            //int c = Int32.TryParseParse(a);
+            int d = Convert.ToInt32(b);
+            //int d = Int32.Parse(b);
+            sum = c + d;
+            string ret = sum.ToString();
+            //int c = Int32.Parse(sum);
+            return ret;
+        }
+        static public string Substract(string a, string b)
+        {
+            int sum = 0;
+            int c = Convert.ToInt32(a);
+            //int c = Int32.TryParseParse(a);
+            int d = Convert.ToInt32(b);
+            //int d = Int32.Parse(b);
+            sum = c - d;
+            string ret = sum.ToString();
+            //int c = Int32.Parse(sum);
+            return ret;
+        }
+        static public string Dziel(string a, string b)
+        {
+            int sum = 0;
+            int c = Convert.ToInt32(a);
+            //int c = Int32.TryParseParse(a);
+            int d = Convert.ToInt32(b);
+            //int d = Int32.Parse(b);
+            sum = c / d;
+            string ret = sum.ToString();
+            //int c = Int32.Parse(sum);
+            return ret;
+        }
+        static public string Mnoz(string a, string b)
+        {
+            int sum = 0;
+            int c = Convert.ToInt32(a);
+            //int c = Int32.TryParseParse(a);
+            int d = Convert.ToInt32(b);
+            //int d = Int32.Parse(b);
+            sum = c * d;
+            string ret = sum.ToString();
+            //int c = Int32.Parse(sum);
+            return ret;
+        }
+
+        static public string CharCheck(char whatis, string a, string b)
+        {
+            switch (whatis)
+            {
+                case '+':
+                    return Add(a, b);
+                //break;
+                case '-':
+                    return Substract(a, b);
+                //break;
+                case '*':
+                    return Mnoz(a, b);
+                //break;
+                case '/':
+                    return Dziel(a, b);
+                //break;
+                default:
+                    return a;
+                    //reak;
+            }
+
+        }
+    }
+}
